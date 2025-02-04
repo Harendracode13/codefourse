3d4fea4a8edd12d8379c2d15f577ee8c3ec8164f
Harendra Piprotar <piprotarharendra@gmail.com>
2025-02-04 14:53:30 +0530

    2042A Greedy Monocarp

---
+++ P90GreedyMonocarp.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int ans=0;
+
+       vector<int> v(n);
+	   int mixi=v[0];
+		for(int i=0;i<n;i++)
+		{
+			cin>>v[i];
+		}
+		sort(v.begin(),v.end());
+
+		for(int i=0;i<n;i++)
+		{
+			if(k>v[n-i-1])
+		}
+
+	}
+}
